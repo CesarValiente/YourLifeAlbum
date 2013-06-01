a62894ffee2d2b0641f4590ee12f72d6989b69d0
Cesar Valiente <cesar.valiente@gmail.com>
2013-06-01 13:11:02 +0200

    * Project src init

---
+++ src/com/cesar/yourlifealbum/ui/activities/YLAMainFragmentActivity.java
@@ -0,0 +1,14 @@
+package com.cesar.yourlifealbum.ui.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class YLAMainFragmentActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(final Bundle arg0) {
+        // TODO Auto-generated method stub
+        super.onCreate(arg0);
+    }
+
+}
---
+++ src/com/cesar/yourlifealbum/ui/fragments/YLAMainFragment.java
@@ -0,0 +1,17 @@
+package com.cesar.yourlifealbum.ui.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class YLAMainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater,
+            final ViewGroup container, final Bundle savedInstanceState) {
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
